df92f93afc9558b313fc999167c97fd8cfa46c16
opajkovic <oljaolja170@gmail.com>
2023-08-21 22:19:57 +0200

    added page librarians

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/dashboard/Dashboard";
 import AppLayout from "./layout/AppLayout";
+import Librarians from "./pages/librarians/Librarians";
 function App() {
   return (
     <BrowserRouter>
@@ -8,6 +9,7 @@ function App() {
         <Route element={<AppLayout />}>
           <Route index element={<Navigate replace to="/dashboard" />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/librarians" element={<Librarians />} />
         </Route>
         {/* <Route path="*" element={<PageNotFound />} /> */}
       </Routes>
--- src/layout/sidebar/Sidebar.jsx
+++ src/layout/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import "./Sidebar.css"
+import "./Sidebar.css";
 import {
   FaTachometerAlt,
   FaBars,
@@ -10,62 +10,125 @@ import {
   FaCog,
   FaTimes,
   FaAngleRight,
-  FaAngleDown
+  FaAngleDown,
 } from "react-icons/fa";
 import NavItem from "../../components/navItem/navItem";
 import { useState } from "react";
 
 function Sidebar() {
-
-  var [isOpen, setIsOpen] = useState(false)
-  var [isExtendOpen, setIsExtendOpen] = useState(false)
+  var [isOpen, setIsOpen] = useState(false);
+  var [isExtendOpen, setIsExtendOpen] = useState(false);
 
   let changeExpand = () => {
-    if(isExtendOpen == false){
-      setIsOpen(true)
+    if (isExtendOpen == false) {
+      setIsOpen(true);
     }
-    setIsExtendOpen(isExtendOpen => !isExtendOpen )
-  }
+    setIsExtendOpen((isExtendOpen) => !isExtendOpen);
+  };
 
   return (
     <aside className={isOpen ? "sidebar open" : "sidebar closed"}>
       <nav>
-      {isOpen ? <FaTimes className="faTimes" onClick={()=>{setIsOpen(false)}} />  :<FaBars className="faBars" onClick={()=>{setIsOpen(true)}} />}
-      <ul>
-        <li>
-          <NavItem isOpen={isOpen} path="dashboard" text="Dashborad" icon={<FaTachometerAlt className="icon"/>} />
-        </li>
-        <li>
-          <NavItem isOpen={isOpen} path="bibliotekari" text="Bibliotekari" icon={<FaAddressBook className="icon" />} />
-        </li>
-        <li>
-          <NavItem isOpen={isOpen} path="ucenik" text="Ucenik" icon={<FaUsers className="icon" />} />
-        </li>
-        <li>
-          <NavItem isOpen={isOpen} path="evidencijaKnjiga" text="Knjige" icon={<FaCopy className="icon" />} />
-        </li>
-        <li>
-          <NavItem isOpen={isOpen} path="autori" text="Autori" icon={<FaAddressBook className="icon" />} />
-        </li>
-        <li>
-          <NavItem isOpen={isOpen} path="izdateKnjige" text="Izdavanje Knjiga" icon={<FaExchangeAlt className="icon" />} />
-        </li>
-        <li>
-        <div className='navLink' onClick={changeExpand}>
-          <FaExpand className="icon" />
-          { isOpen ? <p>Expand Example <span className="extendArrows">{isExtendOpen ? <FaAngleDown/> : <FaAngleRight/>}</span></p> : <></>}
-        </div>
-        <div className={(isOpen && isExtendOpen) ? "expandText openExpand" : "expandText closedExpand"}>
-          <p>Basic Expand</p>
-          <p>Basic Expand</p>
-          <p>Basic Expand</p>
-        </div>
-        </li>
-        <li>
-          <NavItem isOpen={isOpen} path="settings" text="Settings" icon={<FaCog className="icon" />} />
-        </li>
-      </ul>
-    </nav>
+        {isOpen ? (
+          <FaTimes
+            className="faTimes"
+            onClick={() => {
+              setIsOpen(false);
+            }}
+          />
+        ) : (
+          <FaBars
+            className="faBars"
+            onClick={() => {
+              setIsOpen(true);
+            }}
+          />
+        )}
+        <ul>
+          <li>
+            <NavItem
+              isOpen={isOpen}
+              path="dashboard"
+              text="Dashborad"
+              icon={<FaTachometerAlt className="icon" />}
+            />
+          </li>
+          <li>
+            <NavItem
+              isOpen={isOpen}
+              path="librarians"
+              text="Bibliotekari"
+              icon={<FaAddressBook className="icon" />}
+            />
+          </li>
+          <li>
+            <NavItem
+              isOpen={isOpen}
+              path="ucenik"
+              text="Ucenik"
+              icon={<FaUsers className="icon" />}
+            />
+          </li>
+          <li>
+            <NavItem
+              isOpen={isOpen}
+              path="evidencijaKnjiga"
+              text="Knjige"
+              icon={<FaCopy className="icon" />}
+            />
+          </li>
+          <li>
+            <NavItem
+              isOpen={isOpen}
+              path="autori"
+              text="Autori"
+              icon={<FaAddressBook className="icon" />}
+            />
+          </li>
+          <li>
+            <NavItem
+              isOpen={isOpen}
+              path="izdateKnjige"
+              text="Izdavanje Knjiga"
+              icon={<FaExchangeAlt className="icon" />}
+            />
+          </li>
+          <li>
+            <div className="navLink" onClick={changeExpand}>
+              <FaExpand className="icon" />
+              {isOpen ? (
+                <p>
+                  Expand Example{" "}
+                  <span className="extendArrows">
+                    {isExtendOpen ? <FaAngleDown /> : <FaAngleRight />}
+                  </span>
+                </p>
+              ) : (
+                <></>
+              )}
+            </div>
+            <div
+              className={
+                isOpen && isExtendOpen
+                  ? "expandText openExpand"
+                  : "expandText closedExpand"
+              }
+            >
+              <p>Basic Expand</p>
+              <p>Basic Expand</p>
+              <p>Basic Expand</p>
+            </div>
+          </li>
+          <li>
+            <NavItem
+              isOpen={isOpen}
+              path="settings"
+              text="Settings"
+              icon={<FaCog className="icon" />}
+            />
+          </li>
+        </ul>
+      </nav>
     </aside>
   );
 }
