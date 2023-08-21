a0bfabc9d93c9bb23fded9add5c2e10903c125c3
unknown <benidjokovic12@gmail.com>
2023-08-21 18:33:47 +0200

    Installed react-router and react-icons, maked folders: pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,18 @@
-// import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-// import Dashboard from "./pages/dashboard/Dashboard";
-// import Librarians from "./pages/librarians/Librarians";
-// import Students from "./pages/students/Students";
-// import Books from "./pages/books/Books";
-// import Authors from "./pages/authors/Authors";
-// import Bookings from "./pages/bookings/Bookings";
-// import Settings from "./pages/settings/Settings";
-// import PageNotFound from "./pages/PageNotFound";
-// import AppLayout from "./ui/AppLayout";
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route element={<AppLayout />}>
-//           <Route index element={<Navigate replace to="/dashboard" />} />
-//           <Route path="/dashboard" element={<Dashboard />} />
-//           <Route path="/bibliotekari" element={<Librarians />} />
-//           <Route path="/ucenik" element={<Students />} />
-//           <Route path="/evidencijaKnjiga" element={<Books />} />
-//           <Route path="/autori" element={<Authors />} />
-//           <Route path="/izdateKnjige" element={<Bookings />} />
-//           <Route path="/settingsPolisa" element={<Settings />} />
-//         </Route>
-//         <Route path="*" element={<PageNotFound />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/dashboard/Dashboard";
+import AppLayout from "./layout/AppLayout";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<Navigate replace to="/dashboard" />} />
+          <Route path="/dashboard" element={<>Zdravo\</>} />
+        </Route>
+        {/* <Route path="*" element={<PageNotFound />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-// export default App;
+export default App;
