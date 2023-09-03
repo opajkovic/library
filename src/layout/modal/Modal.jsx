efbe29c66fda789da8e40aea2af459442bf7b338
arben-djokovic <benidjokovic12@gmail.com>
2023-09-03 16:16:34 +0200

    fixed modal closing error

--- src/layout/modal/Modal.jsx
+++ src/layout/modal/Modal.jsx
@@ -5,7 +5,7 @@ export default function Modal({ component, setModalClose, className }) {
 
   return (
     <div>
-      <div onClick={setModalClose} className="closeModal"></div>
+      <div onClick={()=>{setModalClose(false)}} className="closeModal"></div>
       <div className={className ? className : "modal"}>{component}</div>
     </div>
   );
