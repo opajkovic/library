8d96a09804b867f94a9e0e240f1a340e50c19a29
opajkovic <oljaolja170@gmail.com>
2023-09-11 22:14:07 +0200

    dodat .env

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,11 +1,13 @@
-import axios from 'axios'
+import axios from "axios";
+
+const url = import.meta.env.VITE_BACKEND_URL;
 
 export default axios.create({
-  baseURL: 'https://tim4.petardev.live/api/',
+  baseURL: url,
   headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
+    "Content-Type": "application/json",
+    Accept: "application/json",
     // just for test >
-    'Authorization': 'Bearer 18|m6tz6lkk4ppTRAtQeiY8Vg6KQXmKXG2MAQQPZno9'
-  }
+    Authorization: "Bearer 18|m6tz6lkk4ppTRAtQeiY8Vg6KQXmKXG2MAQQPZno9",
+  },
 });
