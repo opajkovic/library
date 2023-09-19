d1c2d7e17496a4a597633087775ff8cf37ef5f17
nenadmne <cosovicnenad14@gmail.com>
2023-09-19 13:52:46 +0200

    Pointer events none na disabled button

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -4,7 +4,7 @@ const Button = (props) => {
   const btnClass = `button-div ${props.className}`;
   return (
     <div className={btnClass}>
-    <button type={props.type} className={props.btn} onClick={props.function}>
+    <button type={props.type} className={props.btn} onClick={props.function} disabled={props.disabled}>
       {props.children}
     </button>
     </div>
