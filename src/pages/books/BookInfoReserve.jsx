78644475950115cbedfafdc4de765d6b9dd6660f
arben-djokovic <benidjokovic12@gmail.com>
2023-09-24 17:01:11 +0200

    reserve book connected with api

--- src/pages/books/BookInfoReserve.jsx
+++ src/pages/books/BookInfoReserve.jsx
@@ -1,18 +1,34 @@
 import { useEffect, useState } from "react";
 import "./BookInfoReserve.css";
 import ProfileTitle from "../../layout/profileTitle/ProfileTitle";
-import { redirect, useLoaderData } from "react-router";
+import { redirect, useLoaderData, useParams } from "react-router";
 import SettingsForm from "../../components/UI/SettingsForm";
 import useInput from "../../hooks/useInput";
+import { LoaderStudents } from "../students/Students";
+import { filterAndMap } from "../../util/Functions";
+import { toast } from "react-toastify";
+import api from "../../api/apiCalls";
 
 const isNotEmpty = (value) => value.trim() !== "";
 
 export default function BookInfoReserve() {
   const [book, setBook] = useState({});
   const [nameIsValid, setNameIsValid] = useState(false);
+  const [selectedStudent, setSelectedStudent] = useState('');
   const fetchedData = useLoaderData();
+  let [students, setStudents] = useState()
+  const {id} = useParams()
 
+  async function fetchStudents() {
+    try {
+      const students = await LoaderStudents();
+      setStudents(students);
+    } catch (error) {
+      console.error("Error fetching students:", error);
+    }
+  }
   useEffect(() => {
+    fetchStudents()
     setBook(fetchedData);
   }, []);
 
@@ -25,11 +41,41 @@ export default function BookInfoReserve() {
   } = useInput(isNotEmpty);
 
   const nameHandler = (value) => {
-    setNameIsValid(value);
+    if(students && filterAndMap(students, selectedStudent)){
+      setNameIsValid(value);
+    }else{
+      setNameIsValid(false);
+    }
   };
 
-  const submitHandler = () => {
-    return null;
+  const submitHandler = async() => {
+    let studentId = filterAndMap(students, selectedStudent)[0]
+    function formatDate(date) {
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+      const day = String(date.getDate()).padStart(2, '0');
+    
+      return `${month}/${day}/${year}`;
+    }
+    
+    // Usage
+    const currentDate = new Date(); // Get the current date
+    const formattedDate = formatDate(currentDate); // Format it as "MM/DD/YYYY"
+    let info = {
+      student_id: studentId,
+      datumRezervisanja: formattedDate
+  }
+    try {
+      const response = await api.post(`/books/${id}/reserve`, info);
+      const responseData = response.data;
+      toast.success(responseData.message);
+    } catch (error) {
+      console.log(error.response)
+      toast.error(error.response.data.data.errors)
+      throw error;
+    }
+    
+    // return null;
   }
 
   let formIsValid = false;
@@ -74,12 +120,13 @@ export default function BookInfoReserve() {
           ]}
           select={[
             {
-              options: [{ name: "Mark Twen" }, { name: "Pero Peric" }],
+              options: students,
               input: {
                 label: "Izaberite učenika koji rezerviše knjigu",
                 type: "text",
                 name: "name",
-                value: ""
+                value: selectedStudent,
+                onChange: (newValue)=>{setSelectedStudent(newValue)}
               },
               validHandler: nameHandler,
             },
