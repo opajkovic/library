5de9b2d8a561821a8d1d9da72bd14fe8fc835a84
nenadmne <cosovicnenad14@gmail.com>
2023-10-09 18:18:18 +0200

    Active reservations - added pagination and search

--- src/pages/rentingBooks/layouts/activeReservation.jsx
+++ src/pages/rentingBooks/layouts/activeReservation.jsx
@@ -1,47 +1,136 @@
-import PageTitle from "../../../components/pageTitle/PageTitle";
-import "../rentingBooks.css";
-import BottomContainer from "../components/BottomContainer";
-import { useLoaderData } from "react-router";
 import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { updateRentingData } from "../../../redux/renting-books";
+import { filterSearchedData } from "../../../redux/actions";
+import PageTitle from "../../../components/pageTitle/PageTitle";
+import BottomContainer from "../components/BottomContainer";
 import api from "../../../api/apiCalls";
 import { auth } from "../../../services/AuthService";
+import "../rentingBooks.css";
+
+const headers = [
+  {
+    headerName: "Naziv knjige",
+    sort: false,
+    dropdown: false,
+    dataKey: "knjiga.title",
+    path: "/books/:id",
+    pathId: "knjiga",
+  },
+  {
+    headerName: "Datum rezervacije",
+    sort: false,
+    dropdown: false,
+    dataKey: "action_date",
+  },
+  {
+    headerName: "Rezervacija ističe",
+    sort: false,
+    dropdown: false,
+    dataKey: "",
+  },
+  {
+    headerName: "Rezervaciju podnio",
+    sort: false,
+    dropdown: false,
+    dataKey: "student.name+student.surname",
+    path: "/students/:id",
+    pathId: "student",
+  },
+  { headerName: "Status", sort: false, dropdown: true, dataKey: "status" },
+];
 
 export default function ActiveReservations() {
-  const headers = [
-    { headerName: "Naziv knjige", sort: false, dropdown: false, dataKey: 'knjiga.title', path: '/books/:id', pathId: 'knjiga' },
-    { headerName: "Datum rezervacije", sort: false, dropdown: false, dataKey: 'action_date' },
-    { headerName: "Rezervacija ističe", sort: false, dropdown: false, dataKey: '' },
-    { headerName: "Rezervaciju podnio", sort: false, dropdown: false, dataKey: 'student.name+student.surname', path: '/students/:id', pathId: 'student' },
-    { headerName: "Status", sort: false, dropdown: true, dataKey: 'status' },
-  ];
-  let fetchedData = useLoaderData()
-  let [activeData, setActiveData] = useState([])
+  const dispatch = useDispatch();
 
-  useEffect(()=>{
-    let aktivne = fetchedData.active
-    setActiveData(aktivne)
-  },[])
+  const [reservation, setReservation] = useState([]);
+  const [searchReservation, setSearchReservation] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const [currentPage, setCurrentPage] = useState(0);
+  const [itemsPerPage, setItemsPerPage] = useState(5);
+  const [resetPagination, setResetPagination] = useState(false);
+
+  const fetchedData = useLoaderData();
+  const searchData = useSelector((state) => state.search.searchData);
+  const reservationData = useSelector((state) => state.rentingBooks);
+
+  const handlePageClick = (selectedPage) => {
+    setCurrentPage(selectedPage.selected);
+  };
+
+  const itemPerPageHandler = (value) => {
+    setItemsPerPage(value);
+  };
+
+  useEffect(() => {
+    dispatch(updateRentingData(fetchedData.active));
+    setReservation(fetchedData.active);
+    setSearchReservation(fetchedData.active);
+  }, []);
+
+  useEffect(() => {
+    if (search !== "") {
+      setReservation(searchData);
+      if (resetPagination) {
+        setCurrentPage(0);
+        setResetPagination(false);
+      }
+    } else {
+      if (reservationData !== null) {
+        setReservation(reservationData);
+      }
+    }
+  }, [search, reservationData]);
+
+  const handleGlobalSearch = (event) => {
+    const searchValue = event.target.value.toLowerCase();
+    setSearch(searchValue);
+    setResetPagination(true);
+    dispatch(filterSearchedData(searchReservation, headers, searchValue));
+  };
+
+  const handleColumnSearch = (headerName, searchValue) => {
+    setSearch(searchValue);
+    setResetPagination(true);
+    dispatch(filterSearchedData(searchReservation, headerName, searchValue));
+  };
+
+  const startIndex = currentPage * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const reservationToDisplay = reservation.slice(startIndex, endIndex);
+  const pageCount = Math.ceil(reservation.length / itemsPerPage);
 
   return (
     <div>
       <PageTitle title="Izdavanje Knjiga" />
-      <BottomContainer tableData={activeData} title="Nova knjiga" headers={headers} />
+      <BottomContainer
+        searchColumn={handleColumnSearch}
+        searchGlobal={handleGlobalSearch}
+        itemsPerPageHandler={itemPerPageHandler}
+        onPageChange={handlePageClick}
+        pageCount={pageCount}
+        tableData={reservationToDisplay}
+        title="Nova knjiga"
+        headers={headers}
+      />
     </div>
   );
 }
+
 export async function LoaderReservations() {
   const isAuthenticated = auth.bibliotekarRole();
   if (isAuthenticated) {
     try {
       const response = await api.get(`/books/reservations`);
       const responseData = response.data.data;
-      console.log(responseData)
       return responseData;
     } catch (error) {
       console.error("Loader function error:", error);
       throw error;
     }
-  }else{
-    return []
+  } else {
+    return [];
   }
 }
