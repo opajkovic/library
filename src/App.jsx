6e6698639c6ae32a8cb5e117b1d67ebd8cd45039
nenadmne <cosovicnenad14@gmail.com>
2023-11-09 18:06:47 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Dashboard from "./pages/dashboard/Dashboard";
 import AppLayout from "./layout/AppLayout";
 function App() {
   return (
@@ -8,25 +7,6 @@ function App() {
         <Route element={<AppLayout />}>
           <Route index element={<Navigate replace to="/dashboard" />} />
           <Route path="/dashboard" element={<>Zdravo Contributors@!\</>} />
-        </Route>
-        {/* <Route path="*" element={<PageNotFound />} /> */}
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
-
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Dashboard from "./pages/dashboard/Dashboard";
-import AppLayout from "./layout/AppLayout";
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route index element={<Navigate replace to="/dashboard" />} />
-          <Route path="/dashboard" element={<>Zdravo\</>} />
         </Route>
         {/* <Route path="*" element={<PageNotFound />} /> */}
       </Routes>
